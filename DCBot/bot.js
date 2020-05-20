cc484b97a69fb8520bb8dc1c166aebda14b302a3
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2020-05-20 14:57:06 +0200

    More bot progress! Small arguments processing added

--- DCBot/bot.js
+++ DCBot/bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const json = require('./klucz.json');
+const odrabiamy = require('../odrabiamy-pl.js');
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.username}!`);
@@ -11,15 +12,17 @@ bot.login(json['token']);
 bot.on('message', msg => {
     if(msg.content.startsWith('!')) {
         console.log('got a valid command');
-        let wiad = msg.content.slice(1);
+        let wiad = msg.content.slice(1).trim();
         let args = wiad.split(/[ ]+/);
         let cmd = args.shift();
-        if(cmd.toLowerCase()==='odrabiamy') {
-
+        if(cmd.toLowerCase() === 'odrabiamy') {
+            console.log('got !odrabiamy');
+            msg.channel.send('odebrano !odrabiamy');
+            odrabiamy.getBooksBySubject(args[0], args[1])
         }
         //else if() wsparcie dla innych komend
         else {
-            msg.channel.send('Niepoprawna komenda, wspierane polecenia:\n- Odrabiamy [args]');
+            msg.channel.send('Niepoprawna komenda, wspierane polecenia:\n- `!Odrabiamy [grade] [subject] [page] [example]`');
         }
     }
 })
