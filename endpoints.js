34c03833d3f865d0fafaea8a64f9bed6c6c6c2d9
dommilosz <dommilosz@gmail.com>
2020-05-20 19:54:34 +0200

    upload

--- endpoints.js
+++ endpoints.js
@@ -70,10 +70,11 @@ server.get('/ex',function(req, res) {
     params = webserver.GetParams(req)
     if(params.hash&&params.username&&params.username.trim()!=""&&auth.CheckHash( params.hash,params.username)){
         res.writeHead(200,{"Content-Type": "text/json; charset=utf-8"})
+         if(params.page&&params.book&&params.exid){
+            res.write(JSON.stringify(odrabiamyjs.GetExercise( odrabiamyjs.getExList(odrabiamyjs.getBookByID(params.klasa,params.book),params.page)),params.exid))
+        }else
         if(params.page&&params.book){
             res.write(JSON.stringify(odrabiamyjs.getExList(odrabiamyjs.getBookByID(params.klasa,params.book),params.page)))
-        }else if(params.page&&params.book&&params.exid){
-            res.write(JSON.stringify(odrabiamyjs.GetExercise( odrabiamyjs.getExList(odrabiamyjs.getBookByID(params.klasa,params.book),params.page)),params.exid))
         }
         
         res.end()
