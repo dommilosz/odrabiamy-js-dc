3d102e0dc2f87b217406c505e92c0a1cd86786cc
dommilosz <dommilosz@gmail.com>
2020-05-27 20:30:17 +0200

    added resend messages after !b[ack] or !c[hoose] with wrong arg

--- DCBot/bot.js
+++ DCBot/bot.js
@@ -55,74 +55,74 @@ bot.on("message", async function (msg) {
 				)} \`\`\`\n!c[hoose] <nazwa>`,
 				msg,
 				"```st\n",
-				"```\n"
+				"```\n",
+				true
 			);
 		} else if (
 			cmd.toLowerCase() === "choose" ||
 			cmd.toLowerCase() === "c"
 		) {
+			args = args.join(" ");
+			args = args.split(/[ ]+[|][ ]+/);
+			let min = Math.ceil(0);
+			let max = Math.floor(args.length);
+			let choosen = args[
+				Math.floor(Math.random() * (max - min) + min)
+			].trim();
 			if (
 				userdata[msg.author.id] &&
 				userdata[msg.author.id] != "none" &&
-				userdata[msg.author.id] != ""
+				userdata[msg.author.id] != "" &&
+				CheckChoosen(choosen, msg)
 			) {
-				args = args.join(" ");
-				args = args.split(/[ ]+[|][ ]+/);
-				let min = Math.ceil(0);
-				let max = Math.floor(args.length);
-				let choosen = args[
-					Math.floor(Math.random() * (max - min) + min)
-				].trim();
-				if (CheckChoosen(choosen, msg)) {
-					//sprawdz jezeli autor wiadomosci zainicjowal wczesniej !o
+				//sprawdz jezeli autor wiadomosci zainicjowal wczesniej !o
 
-					if (userdata[msg.author.id][0] == 0) {
-						//sprawdz czy uzytkownik jest swiezo po wpisaniu !o i wybiera klase
-						let subj = Object.keys(
-							odrabiamy.Ksiazki_Subjects[choosen]
-						);
-						let subj2 = Object.keys(
-							odrabiamy.Ksiazki_Subjects[choosen]
-						);
-						subj.forEach((element, i) => {
-							subj[i] = element;
-							subj2[i] = "+ " + element;
-						});
-						AddChooseState(subj, choosen, msg);
-						SendBotMsg(`<@${msg.author.id}>`, msg);
-						SendBotMsg(`Wybrano \`${choosen}\``, msg);
-						SendBotMsgINCodeBlock(
-							`Wybierz Przedmiot:  \`\`\`diff\n${subj2.join(
-								"\n"
-							)} \`\`\`\n!c[hoose] <nazwa>`,
-							msg,
-							"```diff\n",
-							"```\n"
-						);
-					} else if (userdata[msg.author.id][0] == 1) {
-						//sprawdz czy uzytkownik wybiera przedmiot
-						books = odrabiamy.getBooksBySubject(
-							userdata_prev[msg.author.id][1],
-							choosen
-						);
-						books_arr = [];
-						indexes = [];
-						books.forEach((el, i) => {
-							books_arr.push(`+ ${i} : ` + el.friendly_name);
-							indexes.push(i);
-						});
-						AddChooseState(indexes, choosen, msg);
-						SendBotMsg(`<@${msg.author.id}>`, msg);
-						SendBotMsg(`Wybrano \`${choosen}\``, msg);
-						SendBotMsgINCodeBlock(
-							`Wybierz Ksiazke:  \`\`\`diff\n${books_arr.join(
-								"\n"
-							)} \`\`\`\n!c[hoose] <nazwa> (use ID)`,
-							msg,
-							"```diff\n",
-							"```\n"
-						);
-					}
+				if (userdata[msg.author.id][0] == 0) {
+					//sprawdz czy uzytkownik jest swiezo po wpisaniu !o i wybiera klase
+					let subj = Object.keys(odrabiamy.Ksiazki_Subjects[choosen]);
+					let subj2 = Object.keys(
+						odrabiamy.Ksiazki_Subjects[choosen]
+					);
+					subj.forEach((element, i) => {
+						subj[i] = element;
+						subj2[i] = "+ " + element;
+					});
+					AddChooseState(subj, choosen, msg);
+					SendBotMsg(`<@${msg.author.id}>`, msg);
+					SendBotMsg(`Wybrano \`${choosen}\``, msg);
+					SendBotMsgINCodeBlock(
+						`Wybierz Przedmiot:  \`\`\`diff\n${subj2.join(
+							"\n"
+						)} \`\`\`\n!c[hoose] <nazwa>`,
+						msg,
+						"```diff\n",
+						"```\n",
+						true
+					);
+				} else if (userdata[msg.author.id][0] == 1) {
+					//sprawdz czy uzytkownik wybiera przedmiot
+					books = odrabiamy.getBooksBySubject(
+						userdata_prev[msg.author.id][1],
+						choosen
+					);
+					books_arr = [];
+					indexes = [];
+					books.forEach((el, i) => {
+						books_arr.push(`+ ${i} : ` + el.friendly_name);
+						indexes.push(i);
+					});
+					AddChooseState(indexes, choosen, msg);
+					SendBotMsg(`<@${msg.author.id}>`, msg);
+					SendBotMsg(`Wybrano \`${choosen}\``, msg);
+					SendBotMsgINCodeBlock(
+						`Wybierz Ksiazke:  \`\`\`diff\n${books_arr.join(
+							"\n"
+						)} \`\`\`\n!c[hoose] <nazwa> (use ID)`,
+						msg,
+						"```diff\n",
+						"```\n",
+						true
+					);
 				} else if (userdata[msg.author.id][0] == 2) {
 					//sprawdz czy uzytkownik wybiera ksiazke
 					books = odrabiamy.getBooksBySubject(
@@ -139,7 +139,8 @@ bot.on("message", async function (msg) {
 						)} \`\`\`\n!c[hoose] <nazwa>`,
 						msg,
 						"```diff\n",
-						"```\n"
+						"```\n",
+						true
 					);
 				} else if (userdata[msg.author.id][0] == 3) {
 					//sprawdz czy uzytkownik wybiera strone
@@ -164,7 +165,8 @@ bot.on("message", async function (msg) {
 						)} \`\`\`\n!c[hoose] <nazwa>`,
 						msg,
 						"```diff\n",
-						"```\n"
+						"```\n",
+						true
 					);
 				} else if (userdata[msg.author.id][0] == 4) {
 					//wyslij zadanie
@@ -187,18 +189,19 @@ bot.on("message", async function (msg) {
 						SendEx(msg, base64);
 					});
 				} else {
+					ResendMessage(msg, userdata[msg.author.id][0]);
 					SendBotMsg(
 						`ERROR 404. Co ty wpisales? \`${choosen}\``,
 						msg
 					);
 				}
-			} else {
-				SendBotMsg(`EJ EJ EJ. Czy ty wpisales \`!o[drabiamy]\`?`, msg);
+			}else{
+				ResendMessage(msg, userdata[msg.author.id][0]);
+					SendBotMsg(
+						`ERROR 404. Co ty wpisales? \`${choosen}\``,
+						msg
+					);
 			}
-			//args[Math.floor(Math.random() * (min - max) + min)]
-		} else if (cmd.toLowerCase() === "back" || cmd.toLowerCase() === "b") {
-			BackChoosen(msg);
-			SendBotMsg(`BACKED`, msg);
 		} else if (cmd.toLowerCase() === "url") {
 			args = args.join(" ");
 			args = args.split(/[ ]+[|][ ]+/);
@@ -213,6 +216,9 @@ bot.on("message", async function (msg) {
 			odrabiamy.GetEX(choosen).then((base64) => {
 				SendEx(msg, base64);
 			});
+		} else if (cmd.toLowerCase() === "back" || cmd.toLowerCase() === "b") {
+			BackChoosen(msg);
+			ResendMessage(msg, userdata[msg.author.id][0]);
 		}
 		//else if() wsparcie dla innych komend
 		else {
@@ -223,7 +229,6 @@ bot.on("message", async function (msg) {
 		}
 	}
 });
-
 function AddChooseState(valid_options, choosen, msg) {
 	let data = userdata[msg.author.id];
 	if (!userdata_prev[msg.author.id]) userdata_prev[msg.author.id] = [];
@@ -241,13 +246,17 @@ function CheckChoosen(choosen, msg) {
 	if (data[1][lastid].includes(choosen)) {
 		return true;
 	}
+	try{
+	if (data[1][lastid].includes(parseInt(choosen))) {
+		return true;
+	}}catch{}
 	return false;
 }
 function BackChoosen(msg) {
 	let data = userdata[msg.author.id];
 	if (!data) data = [0, []];
 	lastid = data[0];
-	if (lastid >= 0) {
+	if (lastid > 0) {
 		data[0] = data[0] - 1;
 		data[1] = data[1].slice(0, lastid);
 		userdata_prev[msg.author.id] = userdata_prev[msg.author.id].slice(
@@ -266,18 +275,28 @@ function SetChoosen(msg, data) {
 	ClearChoosen(msg);
 	userdata[msg.author.id] = data;
 }
-SendBotMsg = function (content, msg) {
+SendBotMsg = function (content, msg, log) {
 	if (!messages[msg.author.id]) messages[msg.author.id] = [];
-	messages[msg.author.id].push([0, content]);
+	if(!messages[msg.author.id][userdata[msg.author.id][0]]){
+		messages[msg.author.id].push("");
+		messages[msg.author.id].push("");
+		messages[msg.author.id].push("");
+	}
+	if (log && log == true) messages[msg.author.id][userdata[msg.author.id][0]]=([0, content]);
 
 	splitcontent = SplitContent(content);
 	splitcontent.forEach((element) => {
 		msg.channel.send(element);
 	});
 };
-SendBotMsgINCodeBlock = function (content, msg, before, after) {
+SendBotMsgINCodeBlock = function (content, msg, before, after, log) {
 	if (!messages[msg.author.id]) messages[msg.author.id] = [];
-	messages[msg.author.id].push([1, [content, before, after]]);
+	if(!messages[msg.author.id][userdata[msg.author.id][0]]){
+		messages[msg.author.id].push("");
+		messages[msg.author.id].push("");
+		messages[msg.author.id].push("");
+	}
+	if (log && log == true) messages[msg.author.id][userdata[msg.author.id][0]]=([1, [content,before,after]]);
 
 	splitcontent = SplitContent(content);
 	splitcontent.forEach((element, i) => {
@@ -315,15 +334,15 @@ SplitContent = function (content) {
 
 ResendMessage = function (msg, index) {
 	try {
-		if (messages[index][0] == 0) {
-			SendBotMsg(messages[index][1], msg);
+		if (messages[msg.author.id][index][0] == 0) {
+			SendBotMsg(messages[msg.author.id][index][1], msg);
 		}
-		if (messages[index][0] == 1) {
+		if (messages[msg.author.id][index][0] == 1) {
 			SendBotMsg(
-				messages[index][1][0],
+				messages[msg.author.id][index][1][0],
 				msg,
-				messages[index][1][1],
-				messages[index][1][2]
+				messages[msg.author.id][index][1][1],
+				messages[msg.author.id][index][1][2]
 			);
 		}
 	} catch {}
